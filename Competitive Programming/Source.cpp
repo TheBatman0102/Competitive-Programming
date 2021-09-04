cfe1557f3a1d39b8bf889458e180d71e203e2480
Thanh Long Lê Viết <thanhlongtdk2000@gmail.com>
2021-09-05 00:17:45 +0700

    final ver

--- Competitive Programming/Source.cpp
+++ Competitive Programming/Source.cpp
@@ -29,10 +29,5 @@ void main() {
 	ios_base::sync_with_stdio(false);
 
 	int t; cin >> t;
-	//this is branch1
-	2
-	// Helooooooooooooooooooo
-	//asjdasjdlasflsdfj
-	//du ma concacdfdf
-	//fgkfdlhlgfhl
+	//this is merged
 }
